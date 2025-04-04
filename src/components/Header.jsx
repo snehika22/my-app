70ca98d153c80239daabc3755336d44cd3c58901
sneika22 <snhikab22@gmail.com>
2025-04-04 09:48:20 +0530

    add

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { appContext } from "../App";
+export default function Header() {
+  const { user, setUser, cart, products,orders } = useContext(appContext);
+  const items = products.filter((value) => cart[value.id] > 0);
+  console.log(items);
+  return (
+    <div className="App-Header-Row">
+      <h2>React Store</h2>
+      <div>
+        <Link to={"/"}>Home</Link>-
+        <Link to={"/cart"}>Cart({items.length})</Link>-
+        <Link to={"/orders"}>Orders({orders.length})</Link>-
+        {user.email === "" || (!user.email) ? (
+          <Link to={"/login"}>Login</Link>
+        ) : (
+          <Link
+            to={"/login"}
+            onClick={() =>
+              setUser({ ...user, name: "", email: "", password: "" })
+            }
+          >
+            Logout
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+}
